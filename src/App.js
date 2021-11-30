74c6aecbff298c506949852cecd1383ea493a026
JDevMen <jdmendoza_90@hotmail.com>
2021-11-30 11:08:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar } from "react-bootstrap";
+import logo from "./images/video-camera.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="info" variant="light">
+        <Navbar.Brand>
+          <img src={logo} alt="movie camera icon" />
+          Movies
+        </Navbar.Brand>
+      </Navbar>
     </div>
   );
 }
